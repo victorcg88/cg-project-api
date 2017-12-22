4b82291f2428e31b377141e3824fae021ad76cd5
VictorCG <victorcg88@gmail.com>
2017-12-22 11:31:13 +0100

    Updating listening port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 var app = require('./api/app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
